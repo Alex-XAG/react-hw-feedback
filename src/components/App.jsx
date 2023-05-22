a48c97284f2c6ff09856add40f467e794b1835d9
Oleksii Khomiakov <alex.xag1987@gmail.com>
2023-05-22 07:21:33 +0300

    updating app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,40 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+import { FeedbackMarkup } from './FeedbackMarkup/FeedbackMarkup';
+
+export class App extends React.Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  countTotalFeedback = () => {
+    this.props.total = this.props.total + 1;
+  };
+
+  handleClick = ({ target }) => {
+    this.setState(
+      prevState => ({
+        [target.name]: prevState[target.name] + 1,
+      }),
+      this.countTotalFeedback
+    );
+  };
+
+  render() {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        <FeedbackMarkup total={0} />
+      </div>
+    );
+  }
+}
